449b3db2a28397468b798f229f8cf929c8db649a
Pablo Valor <pablovalor89@gmail.com>
2023-06-08 07:43:13 -0300

    remove footer link

--- src/routes/layout/footer/index.tsx
+++ src/routes/layout/footer/index.tsx
@@ -4,11 +4,7 @@ const Footer = () => {
     return (
     <footer className="bg-slate-900 text-slate-200 h-28 flex items-center place-content-center">
         <h3 className="font-bold">
-            <a href="https://github.com/PabloValor/qu-beyond-challenge" target="_blank" className="hover:underline hover:underline-offset-2">
-                <FaGithub className="inline-block mr-1" />
-                Check repo 
-            </a>
-            {` | Pablo Valor | QuBeyond Challenge - 2023`} 
+            {`Pablo Valor | QuBeyond Challenge - 2023`} 
         </h3>
     </footer>
 
