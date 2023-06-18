efbbf6714101cc1cbbcfc38dacf359a1392fa561
Pablo Valor <pablovalor89@gmail.com>
2023-06-18 03:16:26 -0300

    fix Error component click

--- src/components/planets/planets.tsx
+++ src/components/planets/planets.tsx
@@ -21,7 +21,7 @@ export default function Planets() {
     const page = subtractPage(url) 
     setPage(page)
     setSearchParams({p: page})
-    window.scrollTo(0, 0)
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
   }
 
   return (
--- src/components/util/error/index.tsx
+++ src/components/util/error/index.tsx
@@ -1,15 +1,15 @@
 import { BiErrorAlt } from 'react-icons/bi'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Button } from '../button'
 
 export const Error = ({ message } : { message: string })  => {
     return (
-        <div className={`bg-red-500 p-2 font-bold text-center shadow-md rounded-md text-xl text-slate-50`}>
-            <h3>
+        <div className={`bg-red-400 p-2 font-bold text-center shadow-md rounded-md text-xl text-slate-50`}>
+            <h3 className='mb-2'>
                 <BiErrorAlt className={`inline-block mr-1 text-3xl`}/>
                 {message}
             </h3>
-            <Link to={'/ '}>
+            <Link to={'#'} onClick={() => { const navigate = useNavigate(); navigate(-1)}}>
                 <Button>
                     Home
                 </Button>
