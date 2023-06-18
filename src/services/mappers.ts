1aa1ed00d2083523e9371b62e107c991c1c5d47b
Pablo Valor <pablovalor89@gmail.com>
2023-06-18 02:55:17 -0300

    refactor mapper

--- src/services/mappers.ts
+++ src/services/mappers.ts
@@ -3,11 +3,10 @@ import { Planet, PlanetResponse } from "../types/planet";
 export const mapPlanets = (planets: PlanetResponse) => { 
 
     planets.results.map( planet => { 
-        planet.id = planet.url.match(/\/([^\/]+)\/$/)?.[1]
+        mapPlanet(planet)
     })
 }
 
 export const mapPlanet = (planet: Planet) => { 
-
     planet.id = planet.url.match(/\/([^\/]+)\/$/)?.[1]
 }
